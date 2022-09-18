5aa4f3d12139b504f1e31df670c822422d03eee1
JironBach <junshimo2@gmail.com>
2022-09-19 03:17:27 +0900

    eclipseでリクエストとパス対応

--- eclipse/katagiri/src/main/java/com/example/demo/KatagiriApplication.java
+++ eclipse/katagiri/src/main/java/com/example/demo/KatagiriApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +15,14 @@ public class KatagiriApplication {
         SpringApplication.run(KatagiriApplication.class, args);
     }
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+    @GetMapping("/hello/{name}")
+    public String hello_path(@PathVariable("name") String name) {// String name) {
+    	return String.format("Hello %s!", name);
     }
+    
+    @GetMapping("/hello")
+    public String hello_request(@RequestParam(value = "name", defaultValue = "World") String name) {
+    return String.format("Hello %s!", name);
+    }
+    
 }
