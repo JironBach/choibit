d3e3f39032582cb48c6fbdc6f2e7137fc2686f1f
JironBach <junshimo2@gmail.com>
2022-09-20 22:06:31 +0900

    make shimoda3

---
+++ eclipse/shimoda2.obsolete/src/main/java/org/springframework/boot/SpringApplication.java
@@ -0,0 +1,11 @@
+package org.springframework.boot;
+
+import com.example.demo.ShimodaApplication;
+
+public record SpringApplication() {
+
+	public static void run(Class<ShimodaApplication> class1, String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+	}
+
+}
